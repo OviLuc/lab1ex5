f97b96478c5a068891977e6d48cb19a1a6ab399a
Lucaci <Lucaci@DESKTOP-0E3OOL2>
2022-12-05 21:52:24 +0200

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Scrieti un program care citind 2 numere de la tastatura le va afisa tot timpul in ordine crescatoare */
+          
+            Console.WriteLine("Acesat program va ordona numerele in ordine crescatoare");
+
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x >= y)
+            {
+                Console.WriteLine(y +" "+ x );
+            }
+            else
+            {
+                Console.WriteLine(x +" " + y);
+            }
+        }
+    }
+}
